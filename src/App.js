855eac0188714a9ddf619199c81a8a46b11d87be
Etishagarg <gargetisha@gmail.com>
2023-10-27 19:30:31 +0530

    Reviews carousel

--- src/App.js
+++ src/App.js
@@ -1,25 +1,66 @@
-import logo from './logo.svg';
-import './App.css';
+import Slider from "react-slick";
+import "slick-carousel/slick/slick.css"; 
+import "slick-carousel/slick/slick-theme.css";
 
 function App() {
+  const settings = {
+    dots: true,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 3,
+    slidesToScroll: 1
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='w-3/4 m-auto'>
+      <div className="mt-20">
+      <Slider {...settings}>
+        {data.map((d) => (
+          <div key={d.name} className="bg-white h-[450px] text-black rounded-xl">
+            <div className='h-56 bg-indigo-500 flex justify-center items-center rounded-t-xl'>
+              <img src={d.img} alt="" className="h-44 w-44 rounded-full"/>
+            </div>
+
+            <div className="flex flex-col items-center justify-center gap-4 p-4">
+              <p className="text-xl font-semibold">{d.name}</p>
+              <p className="text-center">{d.review}</p>
+              <button className='bg-indigo-500 text-white text-lg px-6 py-1 rounded-xl'>Read More</button>
+            </div>
+          </div>
+        ))}
+      </Slider>
+      </div>
+      
     </div>
   );
 }
+
+const data = [
+  {
+    name: `John Morgan`,
+    img: `/students/John_Morgan.jpg`,
+    review: `Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.`
+  },
+  {
+    name: `Ellie Anderson`,
+    img: `/students/Ellie_Anderson.jpg`,
+    review: `Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.`
+  },
+  {
+    name: `Nia Adebayo`,
+    img: `/students/Nia_Adebayo.jpg`,
+    review: `Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.`
+  },
+  {
+    name: `Rigo Louie`,
+    img: `/students/Rigo_Louie.jpg`,
+    review: `Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.`
+  },
+  {
+    name: `Mia Williams`,
+    img: `/students/Mia_Williams.jpg`,
+    review: `Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.`
+  },
+  
+];
 
 export default App;
